1147427d4e6c50fe1e61c6e327cce284e29cdfd3
zzzonki <zzzonki@gmail.com>
2019-12-04 19:25:34 +0300

    adaptive

--- code.js
+++ code.js
@@ -18,15 +18,26 @@ slider.find('.slider-arrow-left').click(function(){
         showIndex--
     }
 })
+// slider.find('.slider-arrow-right').click(function(){
+//     console.log(showIndex)
+//     if(showIndex >= 4){
+//         sliderItems.eq(showIndex).fadeOut()
+//         showIndex = 0
+//         sliderItems.eq(showIndex).css('display', 'flex')
+//     } else{
+//         sliderItems.eq(showIndex).fadeOut()
+//         showIndex++
+//         sliderItems.eq(showIndex).css('display', 'flex')
+//     }
+// })
+
 slider.find('.slider-arrow-right').click(function(){
-    console.log(showIndex)
-    if(showIndex >= 4){
+    if(showIndex >= sliderItems.length - 1){
         sliderItems.eq(showIndex).fadeOut()
         showIndex = 0
         sliderItems.eq(showIndex).css('display', 'flex')
     } else{
         sliderItems.eq(showIndex).fadeOut()
-        showIndex++
-        sliderItems.eq(showIndex).css('display', 'flex')
+        sliderItems.eq(++showIndex).css('display', 'flex')
     }
 })
