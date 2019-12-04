2141d6e187e1124b3e6fbdf9e30ea52880b0bced
zzzonki <zzzonki@gmail.com>
2019-12-04 20:32:59 +0300

    left arrow

--- code.js
+++ code.js
@@ -1,21 +1,38 @@
 let slider = $(".slider")
-let sliderItems = slider.find(".slider-item")
+let sliderLenta = slider.find(".slider-lenta")
+let sliderItems = slider.find(".slider-lenta-item.real")
+sliderLenta.css('width', (sliderItems.length+2) * 100 + "%")
 
 // MVC
 // Model
 let showIndex = 0
 
 slider.find('.slider-arrow-left').click(function(){
-    console.log(showIndex)
+    // Обрывает выполнение функции во время анимции
+    // Таким образом нельзя мотать слишком быстро
+    if(sliderLenta.hasClass("animated")){
+        return
+    }
+    sliderLenta.addClass('animated')
     if(showIndex <= 0){
-        sliderItems.eq(showIndex).fadeOut()
-        // sliderItems[showIndex]
-        showIndex = sliderItems.length - 1
-        sliderItems.eq(showIndex).css('display', 'flex')
-    } else{
-        sliderItems.eq(showIndex).fadeOut()
-        sliderItems.eq(showIndex-1).css('display', 'flex')
         showIndex--
+        
+        sliderLenta.animate({
+            left: -100 * (showIndex+1)+"%"
+        },1500, function(){
+            showIndex = sliderItems.length-1
+            sliderLenta.css('left', -100 * (showIndex+1) + "%")
+            sliderLenta.removeClass("animated")
+        })
+
+    } else{
+        showIndex--
+        sliderLenta.animate({
+            left: -100 * (showIndex+1)+"%"
+
+        },1500, function(){
+            sliderLenta.removeClass("animated")
+        })
     }
 })
 // slider.find('.slider-arrow-right').click(function(){
@@ -41,3 +58,4 @@ slider.find('.slider-arrow-right').click(function(){
         sliderItems.eq(++showIndex).css('display', 'flex')
     }
 })
+
