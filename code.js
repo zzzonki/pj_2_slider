ac574dfe3b3622724399ef3f8cf1e8e1708a3bec
zzzonki <zzzonki@gmail.com>
2019-12-10 04:55:58 +0300

    готовый слайдер

--- code.js
+++ code.js
@@ -2,12 +2,15 @@ let slider = $(".slider")
 let sliderLenta = slider.find(".slider-lenta")
 let sliderItems = slider.find(".slider-lenta-item.real")
 sliderLenta.css('width', (sliderItems.length+2) * 100 + "%")
+let sliderBullets = slider.find('.slider-bullets div')
 
 // MVC
 // Model
 let showIndex = 0
 
-slider.find('.slider-arrow-left').click(function(){
+changeBullets(showIndex)
+
+slider.find('.slider-arrow-left i').click(function slideLeft(){
     // Обрывает выполнение функции во время анимции
     // Таким образом нельзя мотать слишком быстро
     if(sliderLenta.hasClass("animated")){
@@ -21,10 +24,11 @@ slider.find('.slider-arrow-left').click(function(){
             left: -100 * (showIndex+1)+"%"
         },1500, function(){
             showIndex = sliderItems.length-1
+            changeBullets(showIndex)
             sliderLenta.css('left', -100 * (showIndex+1) + "%")
             sliderLenta.removeClass("animated")
         })
-
+        // bullets
     } else{
         showIndex--
         sliderLenta.animate({
@@ -32,30 +36,73 @@ slider.find('.slider-arrow-left').click(function(){
 
         },1500, function(){
             sliderLenta.removeClass("animated")
+            changeBullets(showIndex)
         })
     }
 })
-// slider.find('.slider-arrow-right').click(function(){
-//     console.log(showIndex)
-//     if(showIndex >= 4){
-//         sliderItems.eq(showIndex).fadeOut()
-//         showIndex = 0
-//         sliderItems.eq(showIndex).css('display', 'flex')
-//     } else{
-//         sliderItems.eq(showIndex).fadeOut()
-//         showIndex++
-//         sliderItems.eq(showIndex).css('display', 'flex')
-//     }
-// })
 
-slider.find('.slider-arrow-right').click(function(){
+slider.find('.slider-arrow-right i').click(function slideRight(){
+    sliderLenta.addClass('animated')
+
     if(showIndex >= sliderItems.length - 1){
-        sliderItems.eq(showIndex).fadeOut()
-        showIndex = 0
-        sliderItems.eq(showIndex).css('display', 'flex')
+        showIndex++
+        sliderLenta.animate({
+            left: -100 * (showIndex+1)+"%"
+        },1500, function(){
+            showIndex = 0
+            sliderLenta.css('left', -100 * (showIndex+1) + "%")
+            sliderLenta.removeClass('animated')
+            changeBullets(showIndex)
+        })
+        if(sliderLenta.hasClass("animated")){
+            return
+        }
     } else{
-        sliderItems.eq(showIndex).fadeOut()
-        sliderItems.eq(++showIndex).css('display', 'flex')
+        showIndex++
+        sliderLenta.animate({
+            left: -100 * (showIndex+1)+"%"
+        },1500, function(){
+            sliderLenta.removeClass('animated')
+            changeBullets(showIndex)
+        })
+        if(sliderLenta.hasClass("animated")){
+            return
+        }
     }
 })
 
+function changeBullets(index) {
+    for (let i = 0; i < sliderBullets.length; i++) {
+        sliderBullets[i].style.backgroundColor = 'rgba(255, 255, 255, 0'
+    }
+    sliderBullets[index].style.backgroundColor = 'white'
+}
+
+document.addEventListener("keypress", (event) => {
+
+    if(event.code == "ArrowLeft"){
+        slideLeft()
+    }
+    else if(event.code == "ArrowRight"){
+        slideRight()
+    }
+})
+
+    for (let i = 0; i < sliderBullets.length; i++) {
+        sliderBullets[i].onclick = function(){
+            console.log(this.id)
+            showIndex = this.id
+            showIndex++
+            console.log(showIndex)
+            sliderLenta.animate({
+                left: -100 * (showIndex)+"%"
+            },1500, function(){
+                sliderLenta.removeClass('animated')
+                changeBullets(showIndex)
+            })
+            if(sliderLenta.hasClass("animated")){
+                return
+            }
+            showIndex--
+        }    
+    }
